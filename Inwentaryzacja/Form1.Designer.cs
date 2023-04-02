f40a1f998dfa9e760d9c0575689234ed93114c4b
Bobero <tobi0304@interia.pl>
2023-04-02 15:55:34 +0200

    v00001

--- Inwentaryzacja/Form1.Designer.cs
+++ Inwentaryzacja/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(425, 170);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(469, 133);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 25);
+            label1.TabIndex = 1;
+            label1.Text = "LOGIN";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(469, 245);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 25);
+            label2.TabIndex = 3;
+            label2.Text = "HASŁO";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(425, 279);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(150, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LimeGreen;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 64, 64);
+            button1.FlatAppearance.BorderSize = 5;
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(425, 384);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 71);
+            button1.TabIndex = 4;
+            button1.Text = "ZALOGUJ";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(425, 537);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 28);
+            label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(984, 661);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label3;
     }
 }
--- Inwentaryzacja/Form1.cs
+++ Inwentaryzacja/Form1.cs
@@ -2,9 +2,41 @@ namespace Inwentaryzacja
 {
     public partial class Form1 : Form
     {
+        string user = "Admin";
+        string haslo = "Qwert";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == user & textBox2.Text == haslo)
+            {
+                label3.Text = "Zalogowano pomyslnie";
+            }
+            else
+            {
+                label3.Text = "B³¹d logowania";
+                textBox1.Text = default;
+                textBox2.Text = default;
+            }
+
         }
     }
 }
