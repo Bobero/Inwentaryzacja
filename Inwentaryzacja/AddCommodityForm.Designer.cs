61f83879c710fa2ecd0e4a939621788007559c47
Bobero <tobi0304@interia.pl>
2023-05-19 17:06:52 +0200

    .

--- Inwentaryzacja/AddCommodityForm.Designer.cs
+++ Inwentaryzacja/AddCommodityForm.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddCommodityForm";
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label3 = new Label();
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(353, 51);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(145, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(296, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nazwa";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(307, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Ilość";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(353, 91);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(145, 23);
+            numericUpDown1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(307, 137);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Opis";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(353, 134);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(145, 107);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(385, 263);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 7;
+            button1.Text = "Dodaj";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // AddCommodityForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Controls.Add(label3);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "AddCommodityForm";
+            Text = "AddCommodityForm";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private Label label3;
+        private RichTextBox richTextBox1;
+        private Button button1;
     }
 }
